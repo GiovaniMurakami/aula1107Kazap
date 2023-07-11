9553ae154a91adf50fd3a188a7bfd1461063d517
giovaniMurakami <giovani.murakami@outlook.com>
2023-07-11 20:04:38 -0300

    Adicionadado solução exercício 2

---
+++ exercicio2.rb
@@ -0,0 +1,9 @@
+#2- Implemente um programa que verifique se um número é par ou ímpar usando a estrutura condicional if..else.
+
+num = gets.chomp.to_i
+
+if num % 2 == 0
+    puts 'Número par'
+else
+    puts 'Número ímpar'
+end
