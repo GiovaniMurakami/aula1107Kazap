45e1f131d23a91773c9d423d5cd92a08e59521d8
giovaniMurakami <giovani.murakami@outlook.com>
2023-07-11 20:46:22 -0300

    Adicionado solução exercício 11

---
+++ exercicio11.rb
@@ -0,0 +1,17 @@
+#11- Verifique se uma pessoa pode votar, sendo maior de 16 anos e não possuindo título de eleitor suspenso.
+
+idade = gets.chomp.to_i
+
+puts 'Informe se possui título de eleitor [y/n]'
+tituloEleitor = gets.chomp
+    if tituloEleitor == 'y' || tituloEleitor == 'Y'
+        tituloEleitor = true
+    else
+        tituloEleitor = false
+    end
+
+if idade >= 16 && tituloEleitor
+    puts 'Pode votar'
+else
+    puts 'Não pode votar'
+end
