960a6bc133bd0eb17f32414539d5875a51aba87b
giovaniMurakami <giovani.murakami@outlook.com>
2023-07-11 20:57:39 -0300

    Adicionado solução ecercício 15

---
+++ exercicio15.rb
@@ -0,0 +1,5 @@
+#15- Faça um programa que verifique se uma pessoa é maior ou menor de idade utilizando o operador ternário.
+
+idade = gets.chomp.to_i
+
+idade < 18 ? (puts 'Menor de idade') : (puts 'Maior de idade')
